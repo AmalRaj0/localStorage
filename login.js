422fa1f0041dc7e8f9a1b433a5e0f4c9c111d475
AmalRaj0 <amalrajvictus.@gmail.com>
2023-11-18 12:35:58 +0530

    changes

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 function login(){
     const userName=document.getElementById("username").value
     localStorage.setItem("username",userName)
-    // console.log(userName);
+    console.log(userName);
+   if(userName.length>0){
     window.location="./home.html"
+   }
 }
